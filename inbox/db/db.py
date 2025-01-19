bc70274ab4b93af1e0555c05284c0c3f1712d728
slashtechno <77907286+slashtechno@users.noreply.github.com>
2025-01-18 22:13:12 -0500

    implement where in read_data

--- inbox/db/db.py
+++ inbox/db/db.py
@@ -1,4 +1,4 @@
-from sqlmodel import Field, SQLModel, create_engine, Session, select
+from sqlmodel import Field, SQLModel, col, create_engine, Session, select
 from inbox import settings
 
 
@@ -18,16 +18,17 @@ def create_db_and_tables():
 
 def create_test_message():
     # Create a Message object
-    test_message = Message(text="Hello, SQL!")
+    test_messages = [Message(text="Hello, SQL!"), Message(text="Hi, SQL!")]
     with Session(engine) as session:
         # Add the object to the session
-        session.add(test_message)
+        [session.add(m) for m in test_messages]
         session.commit()
         # access a single field that'll be refreshed (https://sqlmodel.tiangolo.com/tutorial/automatic-id-none-refresh/#print-a-single-field)
-        print(f"DEBUG: test_message text: {test_message.text}")
+        print(f"test_message text: {test_messages[0].text}")
         # Explicitly refresh the entire object
-        session.refresh(test_message)
-        print(f"DEBUG: test_message: {test_message}")
+        session.refresh(test_messages[0])
+        print(f"test_message: {test_messages[0]}")
+
 
 def read_data():
     # https://sqlmodel.tiangolo.com/tutorial/select
@@ -35,11 +36,17 @@ def read_data():
         statement = select(Message)
         results = session.exec(statement)
         # results = results.all() # return an array
+        print("Printing all messages")
         for m in results:
-            print(f"DEBUG: message: {m}")
-
-
-
+            print(f"message: {m}")
+        # Filter rows where the text begins with "Hi" and ends with "!"
+        # https://sqlmodel.tiangolo.com/tutorial/where/
+        statement = (
+            select(Message)
+            .where(col(Message.text).startswith("Hi"))  # noqa: F821
+            .where(col(Message.text).endswith("!"))
+        )
+        print(session.exec(statement).all())
 
 
 def main():
